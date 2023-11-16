a9e30a2570b7dbed0c13186fe829efc55e06285d
Wellington Ferreira <141442241+WoodyWell@users.noreply.github.com>
2023-11-16 19:11:48 -0300

    Update projeto#01.py

---
+++ Preparação_HTML_CSS/projeto#01.py
@@ -0,0 +1,10 @@
+#lista com 5 produtos de uma loja de música.
+albuns = ['Ten - Pearl Jam', 'A Rush of Blood to The Head - Coldplay', 'Use Your Illusion II - Guns N Roses', 'Is This It - The Strokes', 'Wish You Were Here - Pink Floyd']
+print('Lista de álbuns:')
+print(albuns)
+
+print('=='*35)
+#lista com anos de nascimento de familiares e amigos.
+ano_nascimento = [1971, 1974, 1994, 1996, 2002]
+print('Lista com os anos de nascimento de familiares e amigos:')
+print(ano_nascimento)
