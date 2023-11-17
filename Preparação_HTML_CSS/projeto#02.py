27049c769bfb107d3ddfa81d24fecd841cb981ed
Wellington Ferreira <141442241+WoodyWell@users.noreply.github.com>
2023-11-16 21:36:22 -0300

    Nova atividade

---
+++ Preparação_HTML_CSS/projeto#02.py
@@ -0,0 +1,9 @@
+lista_musicos = ['Djavan', 'Roberto Carlos', 'Elis Regina', 'Tom Jobim', 'Milton Nascimento', 'Chico Buarque', 'Nara Leão', 'Pitty', 'Simonal', 'Moacir Santos', 'Caetano Veloso', 'Elza Soares', 'Paulinho da Viola', 'Yamandú Costa', 'Gal Costa']
+qtd_elementos = len(lista_musicos)
+print('=='*9)
+print('ANALISANDO LISTA')
+print('=='*9)
+print(f'Quantidade de elementos: {qtd_elementos}')
+print(f'Dado salvo no índice 2: {lista_musicos[2]}')
+print(f'Dado salvo no índice 9: {lista_musicos[9]}')
+print(f'Dado salvo no índice 14: {lista_musicos[14]}')
